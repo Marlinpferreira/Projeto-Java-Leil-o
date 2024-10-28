305207e51bec551e9194ec476189cf63c4bc98c6
Marlinpferreira <marlonpmm@gmail.com>
2024-10-28 19:37:57 -0300

    Funcionalidade: criação da tela vendasVIEW para exibir produtos vendidos

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -1,13 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -100,4 +90,5 @@ public class ProdutosDAO {
         return produtosVendidos;
     }
 }
+
 
